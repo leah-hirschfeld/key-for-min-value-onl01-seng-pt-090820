2338f3163e2e9cc6d160fd3acaae6ed0376a7a1b
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-14 17:03:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     nil
   else name_hash.each do |key, value|
-    if key > infinity
-        value
+    if value > infinity
+        key
   end
   end
   end
